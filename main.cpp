1424a7412508cb60e5b62ff6a0396470a0440d06
Mokhtari Mohammed El Amine <37108394+amine0110@users.noreply.github.com>
2018-12-27 12:18:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ class Avion
     double Pa; // poid de l atterrissage
     double Vp; // comme bien de litre il va prendre dans 1 heur
     double Pb; // poid de bagage
-    double Pm; // poid minimum de decollage et atterrissage
     int N; //nombre de passager
+
 public:
-    Avion (){nom = ""; dist =0; } // constructeur par defaut
-    Avion (string a, double b, double c, double d, double e, double f, double g,double h, int i){nom=a; dist=b; Pv=c; Pl=d; Pa=e; Vp=f; Pb=g;Pm=h; N=i;} //constructeur surchargé
+    Avion (){nom = ""; dist =0; Pv =0; Pl =0; Pa =0;  } // constructeur par defaut
+    Avion (string a, double b, double c, double d, double e, double f, double g, int i){nom=a; dist=b; Pv=c; Pl=d; Pa=e; Vp=f; Pb=g; N=i;} //constructeur surchargé
     string getNom(){return nom;} // accesseur pour le nom
     double getDist(){return dist;} // accesseur pour la durée du trajet
     double getPv(){return Pv;} // accesseur pour le poid de cet avion au vide
@@ -23,7 +23,6 @@ public:
     double getPa(){return Pa;} // accesseur pour le poid de l'atterrissage
     double getVp(){return Vp;} // accesseur pour le consommation de cet avion par heur
     double getPb(){return Pb;} // accesseur pour le poid du bagage
-    double getPm(){return Pm;} // accesseur pour le poid minimal du decollage et atterrissage
     double getN(){return N;} // accesseur pour le nombre des passagers
 
 
@@ -34,7 +33,6 @@ public:
     void setPa(double a){Pa = a;} //mutateur pour le poid de l'atterrissage
     void setVp(double a){Vp = a;} //mutateur pour le consommation de cet avion par heur
     void setPb(double a){Pb = a;} //mutateur pour le poid du bagage
-    void setPm(double a){Pm = a;} //mutateur pour le poid minimal du decollage et atterrissage
     void setN(int a){N = a;} //mutateur pour le nombre des passagers
 
 
@@ -58,13 +56,14 @@ int main()
     double b;
     int c;
 
+	cout << "\n"<<endl;
     cout << "Le nom de l'avion est : " ;
     cin >> a;
     A.setNom(a);
     cout << "La duree de ce trajet en (h) est : " ;
     cin >> b;
     A.setDist(b);
-    cout << "Le poid de cette avion vide en (tone) est : " ;
+    cout << "Le poid de cet avion vide en (tone) est : " ;
     cin >> b;
     A.setPv(b);
     cout << "Le poid max du decollage en (tone) est : " ;
@@ -73,30 +72,34 @@ int main()
     cout << "Le poid max du l'atterrissage en (tone) est : " ;
     cin >> b;
     A.setPa(b);
-    cout << "Le poid minimal du decollage et du attirrissage en (tone) est : ";
-    cin >> b;
-    A.setPm(b);
-    cout << "La consommation de kerosene par cet avion en (tone) est : ";
+    cout << "La consommation de kerosene par cet avion en (tone/heur) est : ";
     cin >> b;
     A.setVp(b);
-    cout << "La poid de tous le bagage en (tone) est : ";
+    cout << "La poid totale du bagage en (tone) est : ";
     cin >> b;
     A.setPb(b);
     cout << "Le nombre des passagers est : ";
     cin >> c;
     A.setN(c);
-    //********************************************
-    if (A.decolage()<A.getPl() && A.decolage()> A.getPm() ) cout << "L'avion peut decoller !" << endl;
-    else cout << "L'avion ne peut pas decoller ! " << endl;
-    if (A.atterrissage()<A.getPa() && A.atterrissage()> A.getPm()) cout << "L'avion peut atterrir !" <<endl;
-    else cout << "L'avion ne peut pas atterrir !" << endl;
-    if (A.Kc()>18 ) cout << "Cet avion ne peut pas prendre ce trajet directement, il faudra qu'il fasse une escale, merci! "<<endl; 
-    cout << "****************************************" << endl;
+    
+    cout << "***********************************************************************************************" << endl;
     cout << "kerosene consomme est : " << A.Kc() << endl;
     cout << "Le poid totale de passager en (tone) est : " << A.Pp() << endl;
     cout << "Le poid qu'il va decoller avec, en (tone) est : " << A.decolage() << endl;
     cout << "Le poid qu'il va atterrir avec, en (tone) est : "<< A.atterrissage() << endl;
-
+    cout << "***********************************************************************************************" << endl;
+    
+    if (A.decolage()<=A.getPl()  ) cout << "L'avion peut decoller !" << endl;
+    else cout << "L'avion ne peut pas decoller ! " << endl;
+    cout << "***********************************************************************************************" << endl;
+    
+	if (A.atterrissage()<=A.getPa() ) cout << "L'avion peut atterrir !" <<endl;
+    else cout << "L'avion ne peut pas atterrir !" << endl;
+    cout << "***********************************************************************************************" << endl;
+    
+	if (A.Kc()>18 ) cout << "Cet avion ne peut pas prendre ce trajet directement. Parce que le volume max de son reservoir et 18 tonnes et nous pour ce trajet nous avons besoin de " << A.Kc() << " tonnes. Alors il faudra qu'il fasse une escale, merci! "<<endl; 
+    
+	cout << "\n"<<endl;
 
 
 
